858d6fb226d17e102646ade439b1e3494055de7f
Librist <Librist@MSI>
2022-10-07 19:56:12 +0200

    Add project files.

---
+++ DataLibrary/DataAccess/Interfaces/IDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.DataAccess.Interfaces
+{
+    public interface IDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
+        Task SaveData<T>(string storedProcedure, T parameters, string connectionStringName);
+    }
+}
