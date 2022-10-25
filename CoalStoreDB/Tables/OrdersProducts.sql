729f6b18d3aedccf7481b0c831cf76c646774b80
piotrjohnderko <piotrjohnderko@gmail.com>
2022-10-25 21:29:51 +0200

    Create database tables #7

---
+++ CoalStoreDB/Tables/OrdersProducts.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[OrdersProducts]
+(
+	[OrderId] INT NOT NULL , 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    PRIMARY KEY ([OrderId],[ProductId]), 
+    CONSTRAINT [FK_OrdersProducts_Orders] FOREIGN KEY ([OrderId]) REFERENCES [Orders]([Id]),
+    CONSTRAINT [FK_OrdersProducts_Products] FOREIGN KEY ([ProductId]) REFERENCES [Products]([Id])
+)
